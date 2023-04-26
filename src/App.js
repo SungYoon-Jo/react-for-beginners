999a74c6c58c1497eb6861c860df2f3fab41a13e
goodday <37609646+SungYoon-Jo@users.noreply.github.com>
2023-04-26 17:21:08 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter as Route, Router, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
